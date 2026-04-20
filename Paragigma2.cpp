583195a539373b2e682d7960a631fe994b71f4e4
NurvyyMelon <adinnaufal9@gmail.com>
2026-04-20 11:15:08 +0700

    menambahkan variabel strng dll sekaligus void

--- Paragigma2.cpp
+++ Paragigma2.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class Barang{
+
+    public :
+    string nama;
+    int jumlah;
+    string kategori;
+    float tanggalproduksi;
+
+    void printData(){
+        cout << "Nama : " << nama << endl;
+        cout << "Jumlah : " << jumlah << endl;
+        cout << "Kategori : " << kategori << endl;
+        cout << "Tanggal Produksi : " << tanggalproduksi << endl;
+   }
+}; //Batas dari method printdata
